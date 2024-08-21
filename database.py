2b309d368669f6751eff1a38c3fc67033c414706
romai <romainkio@gmail.com>
2024-08-21 20:03:38 +0200

    data puissance

--- database.py
+++ database.py
@@ -1,13 +1,26 @@
 import csv
 
+from calculations import puissance_oeil
+
+
 def chercher_bdd_lunette_to_lentille(xl):
     with open('bdd lunette to lentille.csv') as fichier:
         lecteur = csv.reader(fichier,delimiter=';')
         for ligne in lecteur:
-            print(ligne)
             if ligne[0] == xl:
-                print (ligne[1])
                 return ligne[1]
     return None
-x=float(chercher_bdd_lunette_to_lentille("-6"))*3
-print(x)
+puissance_oeil(9,-2,40)
+
+def puissance_oeil(xl,yl,zl):
+    #Transformation de la puissance lunette en lentille
+    #utiliser la BDD pour le faire
+    x_1=float(chercher_bdd_lunette_to_lentille(str(xl)))
+    x_2=float(chercher_bdd_lunette_to_lentille(str(xl+yl)))
+    xs=x_1
+    ys=round(x_2-x_1,2)
+    zs=zl
+    print(xs," ; ",ys," ; ",zs)
+    return xs,ys,zs
+
+puissance_oeil(3.5,-3,40)
